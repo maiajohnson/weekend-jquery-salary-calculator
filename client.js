8ca4d2de4d1b04e2dd80eba4a5a5e129510080fc
Maia Johnson <maiaj1306@gmail.com>
2022-09-24 12:03:25 -0500

    completed the rest of the input values for the function inputIntoTable

--- client.js
+++ client.js
@@ -12,7 +12,6 @@ function readyNow() {
 function inputIntoTable() {
    
     let firstName = $('#firstname').val();
-
     let firstNameOutput = $('#firstnameout');
 
     firstNameOutput.append(`
@@ -20,4 +19,41 @@ function inputIntoTable() {
     `);
 
     $('#firstname').val('');
+
+    let lastName = $('#lastname').val();
+    let lastNameOutput = $('#lastnameout');
+
+    lastNameOutput.append(`
+        <td>${lastName}</td>
+    `);
+
+    $('#lastname').val('');
+
+    let id = $('#id').val();
+    let idOutput= $('#idout');
+
+    idOutput.append(`
+        <td>${id}</td>
+    `);
+
+    $('#id').val('');
+
+    let title = $('#jobtitle').val();
+    let titleOutput = $('#jobtitleout');
+
+    titleOutput.append(`
+        <td>${title}</td>
+    `);
+
+    $('#jobtitle').val('');
+
+    let annualSalary = $('#annualsalary').val();
+    let annualSalaryOutput = $('#annualsalaryout');
+
+    annualSalaryOutput.append(`
+        <td>${annualSalary}</td>
+    `);
+
+    $('#annualsalary').val('');
+
 }
