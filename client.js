4d0b8931c282ce86d9dc023e0869ae60262865d6
Maia Johnson <maiaj1306@gmail.com>
2022-09-24 11:51:04 -0500

    added function inputIntoTable and put first name data into table

--- client.js
+++ client.js
@@ -6,5 +6,18 @@ function readyNow() {
     console.log('in jquery.js');
 
 
-    
+    $('#submitbtn').on('click', inputIntoTable);
+}
+
+function inputIntoTable() {
+   
+    let firstName = $('#firstname').val();
+
+    let firstNameOutput = $('#firstnameout');
+
+    firstNameOutput.append(`
+        <td>${firstName}</td>
+    `);
+
+    $('#firstname').val('');
 }
